522e3a5fcbdcb8ae7e1715c401f38632e9f2dd8b
vyasrc <vyascholayil@gmail.com>
2018-05-07 12:08:08 +0530

    Co-Op Commit
    Coded Co-Op Mode with SIngle Player Mode

---
+++ Assets/Galaxy Shooter/Materials/Scripts/Main_Menu/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+
+	private GameManager _gameManager;
+
+	public void LoadSinglePlayerGame(){
+		SceneManager.LoadScene("Single_Player");
+		Time.timeScale = 1;
+	}
+
+	public void LoadCoOpMode(){
+		SceneManager.LoadScene("Co-Op_Mode");
+		Time.timeScale = 1;
+	}
+}
